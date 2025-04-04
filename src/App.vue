9ed319fd45ed7103cca391524c118c3df2b03c19
dhermes99 <viniciushermes99@gmail.com>
2025-04-04 10:35:33 +0100

    inicial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { reactive } from "vue";
+
+const estado = reactive({
+  contador: 0,
+  email: "",
+  saldo: 5000,
+  transferindo: 0,
+});
+</script>
+
+<template></template>
+
+<style scoped></style>
