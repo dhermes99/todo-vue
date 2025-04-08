56140318c9c23bad9008dea52f0a717f90657d56
dhermes99 <viniciushermes99@gmail.com>
2025-04-08 11:21:28 +0100

    Alterações no código para a calculadora

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <CalculadoraAritmetica />
+  </div>
+</template>
+
+<script>
+import CalculadoraAritmetica from "./components/CalculadoraAritmetica.vue";
+
+export default {
+  components: {
+    CalculadoraAritmetica,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", "Helvetica", "Arial", sans-serif;
+  text-align: center;
+  color: #165f0f;
+  margin-top: 60px;
+  background: linear-gradient(135deg, #0f1010, #5d5457);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: #34495e;
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+input,
+select {
+  font-size: 18px;
+  margin: 10px 0;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #101010;
+  box-shadow: 0px 4px 4px rgba(91, 7, 7, 0.1);
+  background-color: rgba(241, 241, 242, 0.9);
+  width: 220px;
+}
+
+select {
+  font-size: 18px;
+  padding: 10px;
+}
+
+p {
+  font-size: 24px;
+  color: #121212;
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 220px;
+}
+
+p:hover {
+  transform: scale(1.1);
+  transition: transform 0.3s ease-in-out;
+  background-color: rgba(23, 88, 8, 0.7);
+}
+
+button {
+  font-size: 18px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  background-color: #4cd137;
+  color: white;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s, box-shadow 0.3s;
+  width: 220px;
+  margin-top: 20px;
+}
+
+button:hover {
+  background-color: #45b039;
+  box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ src/components/CalculadoraAritmetica.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { reactive } from "vue";
+
+const estado = reactive({
+  num1: 0,
+  num2: 0,
+  operacao: "",
+  resultado: "",
+});
+
+function somar(num1, num2) {
+  estado.resultado = num1 + num2;
+}
+
+function subtrair(num1, num2) {
+  estado.resultado = num1 - num2;
+}
+
+function multiplicar(num1, num2) {
+  estado.resultado = num1 * num2;
+}
+
+function dividir(num1, num2) {
+  estado.resultado = num1 / num2;
+}
+</script>
+
+<template>
+  <body>
+    <header class="col-md-12 text-center pt-3">
+      <h1>Calculadora Aritmetica</h1>
+    </header>
+    <main>
+      <form class="row d-block my-auto">
+        <label class="col-md-6" for="num1">Insira o primeiro numero</label>
+        <input
+          class="col-md-6"
+          v-model="estado.num1"
+          type="number"
+          id="num1"
+          placeholder="insira o 1º numero"
+        />
+        <label class="col-md-6" for="num2">Insira o segundo numero</label>
+        <input
+          class="col-md-6"
+          v-model="estado.num2"
+          type="number"
+          id="num2"
+          placeholder="insira o 2º numero"
+        />
+        <div class="row">
+          <select
+            v-model="estado.operacao"
+            class="col-md-4 text-center mx-auto"
+          >
+            <option value="somar">+</option>
+            <option value="subtrair">-</option>
+            <option value="multiplicar">*</option>
+            <option value="dividir">/</option>
+          </select>
+
+          <p>Resultado: {{ resultado }}</p>
+        </div>
+        <div class="row mt-3">
+          <p v-if="estado.operacao === 'somar'">
+            {{ somar(estado.num1, estado.num2) }} {{ estado.resultado }}
+          </p>
+          <p v-else-if="estado.operacao === 'subtrair'">
+            {{ subtrair(estado.num1, estado.num2) }} {{ estado.resultado }}
+          </p>
+          <p v-else-if="estado.operacao === 'multiplicar'">
+            {{ multiplicar(estado.num1, estado.num2) }}{{ estado.resultado }}
+          </p>
+          <p v-else-if="estado.operacao === 'dividir'">
+            {{ dividir(estado.num1, estado.num2) }} {{ estado.resultado }}
+          </p>
+        </div>
+      </form>
+    </main>
+  </body>
+</template>
+
+<style scoped>
+body {
+  background-color: rgb(133, 129, 134);
+  width: 900px;
+  border-radius: 10px;
+}
+
+input,
+select {
+  width: 560px;
+  height: 50px;
+  margin: 10px;
+  display: inline-block;
+}
+</style>
